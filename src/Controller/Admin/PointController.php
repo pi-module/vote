fcae656d43cfe5a68339acc738d8e067eae478e8
Hossein Azizabadi Farahani <djvoltan@gmail.com>
2020-05-18 14:32:34 +0430

    Update to laminas

--- src/Controller/Admin/PointController.php
+++ src/Controller/Admin/PointController.php
@@ -16,7 +16,7 @@ namespace Module\Vote\Controller\Admin;
 use Pi;
 use Pi\Mvc\Controller\ActionController;
 use Pi\Paginator\Paginator;
-use Zend\Db\Sql\Predicate\Expression;
+use Laminas\Db\Sql\Predicate\Expression;
 
 class PointController extends ActionController
 {
